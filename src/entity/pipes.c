1cf50ac4ed20232dbfb8f65fb2565b8c4ab9710d
Chris Douglas <chris.douglas.210@gmail.com>
2021-05-30 18:22:33 +1000

    Moved pipes macros

--- src/entity/pipes.c
+++ src/entity/pipes.c
@@ -4,22 +4,22 @@
 
 #include "../state.h"
 
-#define GAP_SIZE 175
-#define WIDTH 75
-
 pipes_t* pipes_create(const float x0, const float height) {
   pipes_t* self = (pipes_t*)malloc(sizeof(pipes_t));
-  glm_vec2((float[2]){WIDTH, height}, self->size_bottom);
-  glm_vec2((float[2]){WIDTH, state.window->h - GAP_SIZE - height},
+  glm_vec2((float[2]){PIPE_WIDTH, height}, self->size_bottom);
+  glm_vec2((float[2]){PIPE_WIDTH, state.window->h - PIPE_GAP - height},
            self->size_top);
 
   glm_vec2((float[2]){x0, 0}, self->position_bottom);
-  glm_vec2((float[2]){x0, height + GAP_SIZE}, self->position_top);
+  glm_vec2((float[2]){x0, height + PIPE_GAP}, self->position_top);
 
   return self;
 }
 
 void pipes_update(pipes_t* self, float dt) {
+  self->position_bottom[0] -= PIPE_VELOCITY * dt;
+  self->position_top[0] -= PIPE_VELOCITY * dt;
+
   glm_mat4_identity(self->model_bottom);
   glm_translate(self->model_bottom, (vec3){self->position_bottom[0],
                                            self->position_bottom[1], 0.0f});
--- src/entity/pipes.h
+++ src/entity/pipes.h
@@ -4,6 +4,10 @@
 #include <cglm/vec2.h>
 #include <cglm/mat4.h>
 
+#define PIPE_WIDTH 75
+#define PIPE_GAP 175
+#define PIPE_VELOCITY 150.0f
+
 typedef struct pipes {
   vec2 position_bottom;
   vec2 position_top;
